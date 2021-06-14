2b87220729817fc4b428d9662d7773afcb7185ee
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-14 15:03:43 -0300

    02.07 Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -16,8 +16,32 @@ console.log(peso); //obter 100
 console.log(tdAltura);
 console.log(altura);
 */
+var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / altura * altura;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso Inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    console.log("Altura Inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida";
+    
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+ 
+
+console.log(imc);
 
 
 
