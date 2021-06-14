18363344f76591636585c09968742083c91a4f35
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-14 11:55:53 -0300

    02.01  Buscando dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,24 @@
 var titulo = document.querySelector(".titulo");
-		console.log(titulo);
-		titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+/*console.log(paciente); //tr
+console.log(tdPeso); //td que tem o peso
+console.log(peso); //obter 100
+
+console.log(tdAltura);
+console.log(altura);
+*/
+
+var imc = peso / altura * altura;
+
+
+
+
