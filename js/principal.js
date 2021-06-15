c850cab1563ee2baf0673358a67a7302759a780c
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-15 15:15:52 -0300

    03.02 Estilos com JavaScript

--- js/principal.js
+++ js/principal.js
@@ -22,12 +22,14 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Peso Inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
     }
     
     if(altura <= 0 || altura >= 3.00) {
         console.log("Altura Inválida");
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
     }
     
     if (alturaEhValida && pesoEhValido) {
