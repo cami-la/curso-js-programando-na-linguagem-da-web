0357263bf28013feaabd5ed1254bfbf19b592734
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-17 14:30:32 -0300

    04.06 Adicionando pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -41,7 +41,29 @@ for (var i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
-    window.alert("Apertou no botão!");
+    var form = document.querySelector("#form-adiciona");
+
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = form.nome.value;
+    pacienteTr.appendChild(nomeTd);
+    pesoTd.textContent = form.peso.value;
+    pacienteTr.appendChild(pesoTd);
+    alturaTd.textContent = form.altura.value;
+    pacienteTr.appendChild(alturaTd);
+    gorduraTd.textContent = form.gordura.value;
+    pacienteTr.appendChild(gorduraTd);
+    
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
 });
 
 /*console.log(paciente); //tr
