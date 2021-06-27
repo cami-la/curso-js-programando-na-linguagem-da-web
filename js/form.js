80eb30c8c9667e2fec5dca5766d3a74b24075eb3
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-27 15:48:55 -0300

    05.06 Melhorando ainda mais o código

--- js/form.js
+++ js/form.js
@@ -5,29 +5,13 @@ botaoAdicionar.addEventListener("click", function (event) {
 
     var paciente = obtemPacienteDoFormulario(form);
     
-    var pacienteTr = document.createElement("tr");
-    
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-    
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var pacienteTr = montaTr(paciente);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
+
+    form.reset();
 
 });
 
@@ -40,4 +24,29 @@ function obtemPacienteDoFormulario(form) {
         imc: calculaImc(form.peso.value, form.altura.value)
     }
     return paciente;
+}
+
+function montaTd(paciente) {
+    
+}
+
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd("info-nome", paciente.nome));
+    pacienteTr.appendChild(montaTd("info-peso", paciente.peso));
+    pacienteTr.appendChild(montaTd("info-altura", paciente.altura));
+    pacienteTr.appendChild(montaTd("info-gordura", paciente.gordura));
+    pacienteTr.appendChild(montaTd("info-td", paciente.imc));
+    
+    return pacienteTr;
+}
+
+function montaTd(classe, dado) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
 }
