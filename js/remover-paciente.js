622ead0e00ea3b934a0a5b3c2b810f3e482416ea
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-07-10 12:50:07 -0300

    07.03 Desligando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,7 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
 
-pacientes.forEach(function (paciente) {
-  paciente.addEventListener("dblclick", function() {
-    console.log("Fui clicado com um duplo click!")
-    this.remove();
-  });
+tabela.addEventListener("dblclick", function(event) {
+  var alvoEvento = event.target;
+  var paiDoAlvo = alvoEvento.parentNode;
+  paiDoAlvo.remove();
 });
